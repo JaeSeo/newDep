7583c30ea97617949946e2b17617b34dc5d5d524
jaehyes@JAEHYES.COM <jaehyes@JAEHYES.COM>
2019-08-07 14:01:11 +0900

    second

---
+++ install_dependencies/install_dependencies.sh
@@ -0,0 +1,5 @@
+yum update -y
+sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
---
+++ start/start.sh
@@ -0,0 +1,4 @@
+cd var/www/deploy
+npm install
+npm install -g pm2
+pm2 start app.js
