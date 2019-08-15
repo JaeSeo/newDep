e7babe74b6162b335d1c41baaf5506f03a31d561
jaehyes@JAEHYES.COM <jaehyes@JAEHYES.COM>
2019-08-14 23:48:59 -0700

    second

--- deploy/before.sh
+++ deploy/before.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yum update -y
 iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
+su ec2-user -l -c 'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash'
+su ec2-user -l -c '. ~/.nvm/nvm.sh'
 su ec2-user -l -c 'nvm install node'
