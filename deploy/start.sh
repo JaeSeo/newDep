d34d3acbf08a6147bdc69d0da27e5d90346eb5cd
jaehyes@JAEHYES.COM <jaehyes@JAEHYES.COM>
2019-08-15 10:47:39 -0700

    ten

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-su ec2-user -l -c 'npm install'
-su ec2-user -l -c 'npm install -g pm2'
-su ec2-user -l -c 'pm2 start /home/ec2-user/deploy/app.js'
+npm install
+npm install -g pm2
+pm2 start /home/ec2-user/deploy/app.js
